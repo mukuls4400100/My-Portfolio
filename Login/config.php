33034d719bdab30c2b318efb87549aeda13ed56e
mukuls4400100 <mukuls4400400@gmail.com>
2021-01-08 19:36:48 +0530

    update portfolio

--- Login/config.php
+++
@@ -1,4 +0,0 @@
-<?php 
- $conn=mysqli_connect("localhost","root","","Myproject") or die("Connection not establish");
-
- ?>
--- Login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	// session_start();
-
-	// session_unset();
-	session_Destroy();
-	header("location:login.php");
-
- ?>
